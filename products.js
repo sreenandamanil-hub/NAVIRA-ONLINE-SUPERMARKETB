83a32aa86c380cdb87094ce64898c83cd62b62b2
Sreenanda M Anil <sreenandamanil@gmail.com>
2026-07-20 18:25:11 +0530

    update

---
+++ products.js
@@ -0,0 +1,50 @@
+const products = [
+
+{
+    id:1,
+    category:"cap",
+    name:"Urban Explorer Gear",
+    page:"cap1.html",
+    price:"799",
+    oldPrice:"1299",
+    discount:"38% OFF",
+    rating:"★★★★★",
+    description:"Unisex Premium Cotton Bucket Hat",
+    images:[
+        "cap (26).jpeg",
+        "cap (28).jpeg",
+        "cap (1).jpeg"
+    ]
+},
+
+{
+id:2,
+category:"cap",
+name:"Heritage Street Co.",
+page:"cap2.html",
+price:"849",
+rating:"★★★★★",
+description:"Unisex Premium Embroidered Baseball Cap",
+images:[
+"cap (25).jpeg",
+"cap (23).jpeg",
+"cap (27).jpeg"
+]
+},
+
+{
+id:3,
+category:"cap",
+name:"Coastal Horizon Wear",
+page:"cap3.html",
+price:"899",
+rating:"★★★★★",
+description:"Unisex Vintage Washed Cotton Baseball Cap",
+images:[
+"cap (24).jpeg",
+"cap (21).jpeg",
+"cap (20).jpeg"
+]
+}
+
+];
--- script.js
+++ script.js
@@ -42,10 +42,72 @@ function goBack(){
 }
 function stock() {
     alert("❌ Sorry!\n\nThis product is currently OUT OF STOCK.\nPlease try again later.");
+
+    goBack();
 }
 
 window.onload = function(){
     showImage();
 	
 	
-};
+}
+function searchProducts() {
+
+let text = document.getElementById("search").value.trim().toLowerCase();
+
+if (text === "") {
+    window.location.href = "index.html";
+    return;
+}
+
+let output = "";
+
+products.forEach(function(p){
+
+if(
+    p.name.toLowerCase().includes(text) ||
+    p.category.toLowerCase().includes(text) ||
+    p.price.includes(text) ||
+    p.description.toLowerCase().includes(text)
+){
+
+    p.images.forEach(function(img){
+
+        output += `
+<div class="card" onclick="location.href='${p.page}'">
+
+
+
+    <img src="${img}" class="searchPhoto">
+
+    <h3>${p.name}</h3>
+
+    <p class="rating">★★★★★</p>
+
+    <p>
+        <span class="searchprice">₹${p.price}</span>
+        <span class="searcholdPrice">₹${p.oldPrice}</span>
+    </p>
+
+    <p class="searchdiscount">${p.discount}</p>
+</div>
+`;
+
+    });   // <-- Close forEach here
+
+}         // <-- Close if here
+
+});       // <-- Close products.forEach here
+
+document.getElementById("products").innerHTML = output;
+}
+
+// Show all products when page opens
+searchProducts();
+
+
+
+
+
+
+;
